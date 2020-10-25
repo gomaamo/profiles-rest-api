f9a0e7dd314724c3d6f0cc3900b89d7905129c83
gomaamo <mamadomedhat@gmail.com>
2020-10-25 22:05:19 +0100

    API view created

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView greetings"""
+        an_apiview = [
+            'Hey!',
+            'Hallo!',
+            'Hola!',
+        ]
+
+        return Response({'message':'Hi there!', 'Greetings': an_apiview})
